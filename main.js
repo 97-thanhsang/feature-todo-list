58900a729564da176f026986c2f9893ae0b0229b
Thanh Sang <anhsang31797@example.com>
2024-04-23 16:36:26 +0700

    init app

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-// import html from './core.js';
 
-// const cars = ['A','B','C'];
-// const isSuccess = true;
-// const output = html`
-//     <h1>${isSuccess && 'Thành Công'}</h1>
-//     <ul>
-//         ${cars.map(car => `<li>${car}</li>`)}
-//     </ul>
-// `;
-// console.log(output);
 
 import { attach } from './store.js';
+import App from './component/app.js';
 
-attach(() => '<h1> HELLO WORLD </h1>',document.getElementById('root'));
+attach(App,document.getElementById('root'));
 
--- reducer.js
+++ reducer.js
@@ -3,7 +3,15 @@ const init = {
 }
 
 export default function reducer(state = init,action,args){
+    console.log(action,args);
     switch (action) {    
+        case 'ADD':
+            const [newCar] = args;
+            return {
+                ...state,
+                cars: [...state.cars,newCar]
+            }
+            break;
         default:
             return state;
     }
